be49a756e8cbd40e4e91ae9bbbe79c95df1bc4c4
Vagner Araújo <74484733+Vagnerking@users.noreply.github.com>
2021-05-07 04:46:11 -0300

    Update CacheImage.cs

--- FrameworkBox/CacheImage.cs
+++ FrameworkBox/CacheImage.cs
@@ -11,7 +11,6 @@ namespace FrameworkBox
 {
     public static class CacheImage
     {
-        //hello
 
         private static readonly string appStartLocal = $"{ AppDomain.CurrentDomain.BaseDirectory}";
 
